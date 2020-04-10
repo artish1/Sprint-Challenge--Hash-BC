6e6d6b43bd6afffdb3f50cc7baa36a3add260fec
Mark Artishuk <markyshuk@gmail.com>
2020-04-10 08:45:25 -0700

    Finished ex2, tickets problem

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,9 +1,11 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
-from hashtables import (HashTable,
-                        hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+from hashtables import (
+    HashTable,
+    hash_table_insert,
+    hash_table_remove,
+    hash_table_retrieve,
+    hash_table_resize,
+)
 
 
 class Ticket:
@@ -14,10 +16,17 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
-    route = [None] * length
+    route = [None] * (length - 1)
+    # Insert Tickets
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
 
-    """
-    YOUR CODE HERE
-    """
+    # Order tickets by hash source/destination
+    curr_ticket = hash_table_retrieve(hashtable, "NONE")
+    for i in range(0, len(tickets) - 1):
+        route[i] = curr_ticket
+        curr_ticket = hash_table_retrieve(hashtable, curr_ticket)
 
-    pass
+    # Print just because
+    print(f"Route: {route}")
+    return route
